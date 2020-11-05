1a10c0e877f56fc33b41586360d5ab32ed81083f
Kyra <klz245@nyu.edu>
2020-11-05 12:40:30 -0500

    Created grid class
    Created grid class as prep for A* pathfinding

---
+++ Assets/Testing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        Grid grid = new Grid(3, 3, 10f);
+    }
+
+    private void Update()
+    {
+        
+    } 
+}
