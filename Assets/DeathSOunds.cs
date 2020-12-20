f96e8b047a1c42bf77141c4996f444745e583fd5
Coby Hilelly <cobyhilelly@gmail.com>
2020-12-20 16:18:47 -0500

    Added some sounds
    Could still use a little work, will get on it when I can

---
+++ Assets/DeathSOunds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSounds : MonoBehaviour
+{
+    public AudioSource audioSource;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        if (GameObject.Find("Orange Team Player").GetComponent<Autofire>().isDead == true || GameObject.Find("Orange Team Player (1)").GetComponent<Autofire>().isDead == true
+       || GameObject.Find("Orange Team Player (2)").GetComponent<Autofire>().isDead == true || GameObject.Find("Orange Team Player (3)").GetComponent<Autofire>().isDead == true
+       || GameObject.Find("Orange Team Player (4)").GetComponent<Autofire>().isDead == true || GameObject.Find("Orange Team Player (5)").GetComponent<Autofire>().isDead == true
+       || GameObject.Find("Orange Team Player (6)").GetComponent<Autofire>().isDead == true || GameObject.Find("Orange Team Player (7)").GetComponent<Autofire>().isDead == true
+       || GameObject.Find("Orange Team Player (8)").GetComponent<Autofire>().isDead == true)
+
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+    }
+}
---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip Fire;
+
+    public AudioClip Dead;
+
+    public AudioClip Dead2;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        if (GameObject.Find("Blue Team Player").GetComponent<Autofire>().isFiring == true || GameObject.Find("Blue Team Player (1)").GetComponent<Autofire>().isFiring == true
+            || GameObject.Find("Blue Team Player (2)").GetComponent<Autofire>().isFiring == true || GameObject.Find("Blue Team Player (3)").GetComponent<Autofire>().isFiring == true
+            || GameObject.Find("Blue Team Player (4)").GetComponent<Autofire>().isFiring == true)
+            
+
+        {
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.PlayOneShot(Fire, 1F);
+            }
+        }
+
+        if (GameObject.Find("Blue Team Player").GetComponent<Autofire>().isDead == true || GameObject.Find("Blue Team Player (1)").GetComponent<Autofire>().isDead == true
+           || GameObject.Find("Blue Team Player (2)").GetComponent<Autofire>().isDead == true || GameObject.Find("Blue Team Player (3)").GetComponent<Autofire>().isDead == true
+           || GameObject.Find("Blue Team Player (4)").GetComponent<Autofire>().isDead == true)
+
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.PlayOneShot(Dead, 1F);
+            }
+
+    }
+}
